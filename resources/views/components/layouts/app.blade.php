92021cdb7e3e7d9da8526cf210a74a64fc5a091e
dstrickler <dave@thestricklers.org>
2024-10-28 18:19:50 -0400

    Added  @filamentStyles() and  @filamentScripts()

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,9 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? 'Page Title' }}</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Loads Tailwind -->
     @vite('resources/css/app.css')
+
+    @filamentStyles()
+    @filamentScripts()
+
 </head>
+
 <body>
 {{ $slot }}
 </body>
+
 </html>
